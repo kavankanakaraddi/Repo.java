a0b5f90ab6d97e1b9798ec0948af337b45692d6d
Kavan Kanakaraddi <kavankreddy12@gmail.com>
2023-01-27 13:31:34 +0530

    new constructor

---
+++ Projects/src/com/xworkz/boot/Tile.java
@@ -0,0 +1,7 @@
+package com.xworkz.boot;
+
+public class Tile {
+public Tile() {
+	System.out.println("Running Tile Without Arguments");
+}
+}
---
+++ Projects/src/com/xworkz/things/TileRunner.java
@@ -0,0 +1,17 @@
+package com.xworkz.things;
+
+import com.xworkz.boot.Tile;
+
+public class TileRunner {
+
+	public static void main(String[] args) {
+		new Tile();
+		new Tile();
+		new Tile();
+		new Tile();
+		new Tile();
+		new Tile();
+
+	}
+
+}
